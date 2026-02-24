f927dc2d1ab7c6b2bea3a3961670784d8b291975
Muhammad Ali <official.muhammadali.81@gmail.com>
2026-02-24 10:31:57 +0000

    Datatypes

---
+++ .devcontainer/01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict" ; //treat all js code as a newer version
+//alert(3+3) // we are using nodejs not browser
+//alert("Hitesh Learning"); 
+
+//datatypes
+
+//number = 2 to power 53
+//bigint
+//string =>""
+//Boolean => true/flase
+//null => standalone value
+//undefined =>
+// symbol => unique
+//object
+
+
+
+
+console.log(3+3);
+console.log("Ali");
+
+console.log (typeof null);
+console.log(typeof undefined);
