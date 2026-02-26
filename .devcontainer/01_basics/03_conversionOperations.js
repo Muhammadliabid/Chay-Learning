d97ec4fa9b0778181988603100af427a686bf0f8
Muhammad Ali <official.muhammadali.81@gmail.com>
2026-02-26 11:10:30 +0000

    Converstion of data types

---
+++ .devcontainer/01_basics/03_conversionOperations.js
@@ -0,0 +1,17 @@
+let score = "25"
+
+
+console.log(typeof (score));
+console.log (typeof score);
+
+let valueInNumber = Number (score)
+console.log (typeof valueInNumber);
+console.log (valueInNumber);
+
+let IsloggedIn = "3"
+let booleanIsloggedIn = Boolean(IsloggedIn)
+console.log(booleanIsloggedIn)
+
+let someNumber = 77
+let stringNumber = String (someNumber)
+console.log(stringNumber)
